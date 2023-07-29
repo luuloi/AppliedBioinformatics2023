6f54c715f00bed95e1f3c6f547346305c939769b
Tien Du <52783290+tiendu@users.noreply.github.com>
2023-07-29 08:42:43 +0700

    Update visualization.R

--- Lecture2/data_visualization/script/visualization.R
+++ Lecture2/data_visualization/script/visualization.R
@@ -1,174 +1,142 @@
-# THIS SCRIPT TO VISULIAZTION IN BASE PLOT IN R
+# This script introduces visualization in base plot and ggplot2 in R
 
-# loading library
+# Section 1: Load required libraries
+# Loading necessary libraries for data manipulation and visualization
 library(ggplot2)
 library(reshape2)
 library(vioplot)
 library(ggpubr)
 library(ggdist)
 
-# setup workdir
+# Section 2: Set working directory
+# Setting the working directory
 setwd("/home/ds02/Documents/data_visualization")
-getwd()
+getwd()  # Checking the working directory
 
-# 1. Import data
+# Section 3: Import Data
+# Importing data from a CSV file
 data <- read.csv("data/Heart_Disease_Prediction.csv")
-dim(data)
-head(data)
+dim(data)  # Checking the dimensions of the data
+head(data)  # Displaying the first few rows of the data
 
-## structure of data
+# Section 4: Data structure explanation
+# Understanding the structure of the data
 str(data)
-# 'data.frame':   270 obs. of  15 variables:
-#  $ index                  : int  0 1 2 3 4 5 6 7 8 9 ...
-#  $ Age                    : int  70 67 57 64 74 65 56 59 60 63 ...
-#  $ Sex                    : int  1 0 1 1 0 1 1 1 1 0 ...
-#  $ Chest.pain.type        : int  4 3 2 4 2 4 3 4 4 4 ...
-#  $ BP                     : int  130 115 124 128 120 120 130 110 140 150 ...
-#  $ Cholesterol            : int  322 564 261 263 269 177 256 239 293 407 ...
-#  $ FBS.over.120           : int  0 0 0 0 0 0 1 0 0 0 ...
-#  $ EKG.results            : int  2 2 0 0 2 0 2 2 2 2 ...
-#  $ Max.HR                 : int  109 160 141 105 121 140 142 142 170 154 ...
-#  $ Exercise.angina        : int  0 0 0 1 1 0 1 1 0 0 ...
-#  $ ST.depression          : num  2.4 1.6 0.3 0.2 0.2 0.4 0.6 1.2 1.2 4 ...
-#  $ Slope.of.ST            : int  2 2 1 2 1 1 2 2 2 2 ...
-#  $ Number.of.vessels.fluro: int  3 0 0 1 1 0 1 1 2 3 ...
-#  $ Thallium               : int  3 7 7 7 3 7 6 7 7 7 ...
-#  $ Heart.Disease          : chr  "Presence" "Absence" "Presence" "Absence" ...
 
+# Section 5: Data cleaning
+# Cleaning the data by handling missing values
+table(is.na(data))  # Checking the presence of NA values
+na.omit(data)  # Removing rows with NA values
+complete.cases(data)  # Checking for complete cases (no NA values)
 
-# 2. clean data
-table(is.na(data))
-na.omit(data)
-complete.cases(data)
-
-data[complete.cases(df), ]
-data[rowSums()(is.na(data)) == 0, ]
-data[, colSums(is.na(data) == 0)]
-
-# 1. barplot
-## base plot
+# Section 6: Visualization
+## Barplot
+### Base plot barplot
 barplot(table(data$Sex),
-    ylab = "Number of sample", # add y label
-    xlab = "Sex", # add x label
-    main = "Age count in dataset", # add title for graph
-    col = c("#90bf80", "blue"), # add color
-    fill = c("pink")
+    ylab = "Number of sample",  # Adding y label
+    xlab = "Sex",  # Adding x label
+    main = "Age count in dataset",  # Adding title for graph
+    col = c("#90bf80", "blue"),  # Adding color
+    fill = c("pink")  # Adding fill color
 )
 
-## ggplot2 plot
+### ggplot2 barplot
 ggplot(data, aes(as.factor(Sex), fill = as.factor(Sex))) +
-    geom_bar() + # select a type of plot
-    labs(x = "Sex", y = "Number of sample", title = "Age count in dataset") + # add title, x axis's name, y axis'name
-    guides(fill = guide_legend(title = "Sex")) +
-    theme(text = element_text(size = 20))
+    geom_bar() +  # Selecting bar plot type
+    labs(x = "Sex", y = "Number of sample", title = "Age count in dataset") +  # Adding labels and title
+    guides(fill = guide_legend(title = "Sex")) +  # Adding legend
+    theme(text = element_text(size = 20))  # Setting theme
 
-# 2. histogram
-## base plot
-par(mfrow = c(1, 2))
+## Histogram
+### Base plot histograms
+par(mfrow = c(1, 2))  # Setting up a plotting area for two plots
 hist(data$BP,
-    main = "The blood pressure level",
-    xlab = "Blood pressure level"
+    main = "The blood pressure level",  # Adding main title
+    xlab = "Blood pressure level"  # Adding x label
 )
 hist(data$Cholesterol,
-    main = "The cholesterol level",
-    xlab = "Cholesterol level"
+    main = "The cholesterol level",  # Adding main title
+    xlab = "Cholesterol level"  # Adding x label
 )
-dev.off()
-## ggplot2
-### subset the data
-data_sub <- data[, c("BP", "Cholesterol")]
-head(data_sub)
+dev.off()  # Closing the plotting device
 
-### transpose data: wide format-> long format
-mdata_sub <- melt(data_sub)
-head(mdata_sub)
-
+### ggplot2 histograms
+data_sub <- data[, c("BP", "Cholesterol")]  # Subsetting the data
+mdata_sub <- melt(data_sub)  # Reshaping data from wide to long format
 ggplot(mdata_sub, aes(value, fill = variable)) +
     theme_bw() +
-    geom_histogram() +
-    facet_grid(~variable) +
+    geom_histogram() +  # Adding histogram
+    facet_grid(~variable) +  # Adding facets
     theme(legend.position = "NULL") +
     labs(x = "") +
-    theme(text = element_text(size = 25))
+    theme(text = element_text(size = 25))  # Setting theme
 
-# 3. density plot
-## base plot
-### Compute the density data
-dens <- density(data$BP)
-par(mfrow = c(1, 2))
-### plot density
+## Density plot
+### Base plot density
+dens <- density(data$BP)  # Calculating density of 'BP'
+par(mfrow = c(1, 2))  # Setting up a plotting area for two plots
 plot(dens,
-    frame = FALSE, col = "steelblue",
-    main = "Density plot of Blood pressure"
+    frame = FALSE, col = "steelblue",  # Adding color and removing frame
+    main = "Density plot of Blood pressure"  # Adding main title
 )
-### Fill the density plot using polygon()
 plot(dens,
-    frame = FALSE, col = "steelblue",
-    main = "Density plot of blood pressure"
+    frame = FALSE, col = "steelblue",  # Adding color and removing frame
+    main = "Density plot of blood pressure"  # Adding main title
 )
-polygon(dens, col = "steelblue")
-dev.off()
-## ggplot2
+polygon(dens, col = "steelblue")  # Filling the density plot
+dev.off()  # Closing the plotting device
+
+### ggplot2 density plot
 ggplot(data, aes(BP, fill = Heart.Disease)) +
-    geom_density(alpha = 0.5) +
-    theme_minimal() +
+    geom_density(alpha = 0.5) +  # Adding density plot with transparency
+    theme_minimal() +  # Setting minimal theme
     theme(text = element_text(size = 20)) +
-    guides(fill = guide_legend(title = "Heart Disease"))
+    guides(fill = guide_legend(title = "Heart Disease"))  # Adding legend
 
+## Boxplot and violin plot
+### Base plot boxplot and violin plot
+boxplot(data$Age, col = "white", xlab = "Age", horizontal = T)  # Adding horizontal boxplot
+stripchart(data$Age,  # Adding points to boxplot
+    method = "jitter",  # Adding random noise
+    pch = 19,  # Setting point symbol
+    col = 4,  # Setting color of the symbol
+    add = TRUE  # Adding it over the boxplot
+)
 
-# 4. boxplot and violin plot
-# base plot
-## one variable
-### Horizontal box plot
-boxplot(data$Age, col = "white", xlab = "Age", horizontal = T)
+par(mfrow = c(1, 2))  # Setting up a plotting area for two plots
+boxplot(Age ~ Heart.Disease, data = data, col = "lightblue")  # Adding boxplot
+vioplot(Age ~ Heart.Disease, data = data, col = "lightblue")  # Adding violin plot
+dev.off()  # Closing the plotting device
 
-# Points
-stripchart(data$Age, # Data
-    method = "jitter", # Random noise
-    pch = 19, # Pch symbol
-    col = 4, # Color of the symbol
-    add = TRUE
-) # Add it over
-
-## two variable
-par(mfrow = c(1, 2))
-boxplot(Age ~ Heart.Disease, data = data, col = "lightblue")
-vioplot(Age ~ Heart.Disease, data = data, col = "lightblue")
-dev.off()
-
-
-# ggplot2
+### ggplot2 boxplot and violin plot
 ggplot(data, aes(x = Heart.Disease, y = Age, col = Heart.Disease)) +
-    geom_boxplot(width = 0.3) +
-    stat_compare_means(size = 7) +
-    theme_classic() +
+    geom_boxplot(width = 0.3) +  # Adding boxplot
+    stat_compare_means(size = 7) +  # Adding comparison of means
+    theme_classic() +  # Setting classic theme
     labs(x = "Sex") +
     theme(legend.position = "NULL") +
     theme(text = element_text(size = 20)) +
-    ggdist::stat_halfeye(aes(fill = Heart.Disease), adjust = 0.5, justification = -0.5, width = 0.4)
+    ggdist::stat_halfeye(aes(fill = Heart.Disease), adjust = 0.5, justification = -0.5, width = 0.4)  # Adding half-eye plot
 
-
-# 5. Scatter plot
-## base plot
-x <- data$BP
-y <- data$Cholesterol
+## Scatter plot
+### Base plot scatter plot
+x <- data$BP  # Setting x variable
+y <- data$Cholesterol  # Setting y variable
 plot(x, y,
-    xlab = "Blood pressure",
-    ylab = "Choloesterol",
-    pch = 19, frame = FALSE,
-    col = "blue"
+    xlab = "Blood pressure",  # Adding x label
+    ylab = "Choloesterol",  # Adding y label
+    pch = 19, frame = FALSE,  # Setting point symbol and removing frame
+    col = "blue"  # Setting color
 )
-abline(lm(y ~ x, data = data),
-    col = "black",
-    lwd = 5,
-    lty = 1
+abline(lm(y ~ x, data = data),  # Adding regression line
+    col = "black",  # Setting color of line
+    lwd = 5,  # Setting line width
+    lty = 1  # Setting line type
 )
 
-## ggplot2
+### ggplot2 scatter plot
 ggplot(data, aes(x = BP, y = Cholesterol)) +
-    geom_point(aes(col = Heart.Disease), size = 4, alpha = 0.7) +
-    geom_smooth(method = "lm") +
+    geom_point(aes(col = Heart.Disease), size = 4, alpha = 0.7) +  # Adding points
+    geom_smooth(method = "lm") +  # Adding regression line
     theme(text = element_text(size = 15)) +
-    theme_classic() +
-    labs(x = "Blood Pressure", col = "Heart Disease")
-ggsave(paste0("output", "/save_plot.pdf"))
+    theme_classic() +  # Setting classic
