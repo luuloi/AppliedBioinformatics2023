d71115462aacc70fbd683450aa1061fc21909e97
Tien Du <52783290+tiendu@users.noreply.github.com>
2023-07-29 08:26:38 +0700

    Update import_data.R

--- Lecture2/data_visualization/script/import_data.R
+++ Lecture2/data_visualization/script/import_data.R
@@ -1,51 +1,46 @@
-# THIS SCRIPT SHOWS SOME METHODS TO IMPORT DATA IN R
+# This script demonstrates various methods to import data in R
 
-# set workdir
-setwd("/home/hoangnguyen/Documents/data_visualization")
-getwd() # check workdir
+# Section 1: Set Working Directory
+setwd("/home/hoangnguyen/Documents/data_visualization")  # Set working directory
+getwd()  # Check working directory
 
-# 1. the file is txt format
-data_1 <- read.table("data/other_data/text.txt", sep = "\t", header = T, check.names = F)
-data_2 <- read.table("data/other_data/smoking.txt",sep ="\t",header=T,check.names = F) 
-head(data_2)
+# Section 2: Import .txt Files
+data_1 <- read.table("data/other_data/text.txt", sep = "\t", header = T, check.names = F)  # Import .txt file
+data_2 <- read.table("data/other_data/smoking.txt", sep ="\t", header = T, check.names = F)  # Import another .txt file
+head(data_2)  # Display first few rows of the data
 
-# 2. the file is csv format 
-data_3 <- read.csv("data/other_data/prawnGR.CSV",sep=",")
-head(data_3)
-dt <- read.table("data/other_data/prawnGR.CSV",sep=",")
-dt
-# 3. the file is tsv
-data_4 <- read.delim("data/other_data/Petal.Length.tsv", sep="\t")
-head(data_4)
+# Section 3: Import .CSV Files
+data_3 <- read.csv("data/other_data/prawnGR.CSV", sep=",")  # Import .CSV file
+head(data_3)  # Display first few rows of the data
+dt <- read.table("data/other_data/prawnGR.CSV", sep=",")  # Import .CSV file using read.table()
+dt  # Display the data
 
-# 4. the file is 
-install.packages("xlsx")
-library(xlsx)
-data_4 <- read_excel("data/other_data/whaledata.xls", sheet = 1)
+# Section 4: Import .tsv Files
+data_4 <- read.delim("data/other_data/Petal.Length.tsv", sep="\t")  # Import .tsv file
+head(data_4)  # Display first few rows of the data
 
+# Section 5: Import .xls Files
+install.packages("xlsx")  # Install the 'xlsx' package
+library(xlsx)  # Load the 'xlsx' package
+data_4 <- read_excel("data/other_data/whaledata.xls", sheet = 1)  # Import .xls file
 
-# data.table packages
-library(data.table)
-data_5 <- as.data.frame(fread("data/other_data/Petal.Length.tsv"))
-data_5
-data_6 <- as.data.frame(fread("data/other_data/children.txt"))
-data_6
-data_7 <- as.data.frame(fread("data/other_data/Gigartina.CSV"))
-data_7
+# Section 6: Using data.table Package
+library(data.table)  # Load the 'data.table' package
+data_5 <- as.data.frame(fread("data/other_data/Petal.Length.tsv"))  # Import .tsv file using fread()
+data_5  # Display the data
+data_6 <- as.data.frame(fread("data/other_data/children.txt"))  # Import .txt file using fread()
+data_6  # Display the data
+data_7 <- as.data.frame(fread("data/other_data/Gigartina.CSV"))  # Import .CSV file using fread()
+data_7  # Display the data
 
+# Section 7: Read Multiple Files in R Using apply and reduce
+list_file <- list.files("data/multi_files", pattern = ".tsv", full.names = T)  # List all .tsv files in the directory
 
-# Read multi files in R using apply and reduce
-## list path of all file
-list_file <- list.files("data/multi_files", pattern = ".tsv", full.names = T)
+read_file <- lapply(list_file, function(x) read.delim(x, header = T, sep = "\t"))  # Read all files using lapply()
 
-# read all files
-read_file <- lapply(list_file, function(x) read.delim(x, header = T, sep = "\t"))
+data <- Reduce(function(x, y) merge(x, y, by = "ID"), read_file)  # Merge all files using Reduce()
 
-# merge all files
-data <- Reduce(function(x, y) merge(x, y, by = "ID"), read_file)
+data_rm <- data[!duplicated(as.list(data))]  # Remove duplicate rows
 
-# Remove duplicate species column
-data_rm <- data[!duplicated(as.list(data))]
-
-# save the data
-write.table(data_rm, paste0("output", "/newdata.tsv"), col.names = T, row.names = F, sep = "\t", quote = F)
+# Section 8: Save the Data
+write.table(data_rm, paste0("output", "/newdata.tsv"), col.names = T, row.names = F, sep = "\t", quote = F)  # Save the data to a .tsv file
